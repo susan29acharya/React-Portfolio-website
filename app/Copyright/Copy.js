0ec33abf9d0c5603d04eb7b7fc7ff42a491ff42e
susan acharya <krksusan8@gmail.com>
2024-07-26 22:36:18 +0545

    footer links, icons and servies added

--- app/Copyright/Copy.js
+++ app/Copyright/Copy.js
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import Link from 'next/link'
+import Aboutpage from '../About/page'
+import Home_page from '../Home/page'
+import Skills_page from '../Skills/page'
+import Contact_page from '../Contact/page'
 const Copyright = () => {
   return (
     <>
     <div className='copyright'>
-        <p>---- Copyright.Susanacharya.&copy;*2024* ----</p>
+        
+        <div className='services'>
+          <h3>Services</h3>
+          <ul>
+            <li>Web Development</li>
+            <li>App Development</li>
+            <li>Python Software</li>
+
+          </ul>
+        </div>
+
+        <div className='useful-links'>
+          <h3>Useful Links</h3>
+        <ul>
+            <li><Link href="/Home" className='links footer-link'>Home</Link></li>
+            <li><Link href="/About" className='links footer-link'>About</Link></li>
+            <li><Link href="/Skills" className='links footer-link'>Skills</Link></li>
+            <li><Link href="/Contact" className='links footer-link'>Contact</Link></li>
+
+          </ul>
+        </div>
+
+        <div className='icons'>
+          <h3>Social Media</h3>
+        </div>
     </div>
     </>
   )
